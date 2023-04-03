7201b977ab3db980b9d167efe822525b7f0782e8
ARYAN SINGLA <72137517+aryansingla@users.noreply.github.com>
2023-04-03 14:36:11 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -3,19 +3,22 @@ import cors from 'cors';
 import express from 'express';
 import morgan from 'morgan';
 import connectDb from './config/db.js';
-
+import authRoute from './routes/authRoute.js';
 
 dotenv.config();
-
 connectDb();
 
+// rest object
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'))
-
 const PORT = process.env.PORT || 5000;
+
+// routes
+app.use('/api/v1/auth', authRoute);
+
 app.get("/", (req, res) => {
     res.send("API is running...");
 });
