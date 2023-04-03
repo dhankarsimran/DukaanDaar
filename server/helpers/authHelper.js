de0f24cf2a8af6162543edd1d2b9744fc955c6ca
ARYAN SINGLA <72137517+aryansingla@users.noreply.github.com>
2023-04-03 16:54:37 +0530

    middleware added

--- server/helpers/authHelper.js
+++ server/helpers/authHelper.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcrypt';
 
-export const hashPassword = async ()=>{
+export const hashPassword = async (password)=>{
     try {
         const saltRounds = 10;
         const hashedPassword = await bcrypt.hash(password, saltRounds);
--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,10 +1,17 @@
 import express from 'express';
-import { registerController } from '../controllers/authController.js';
+import { loginController, registerController, testController } from '../controllers/authController.js';
+import { isAdmin, requireSignIn } from '../middlewares/authMiddleware.js';
 // router object
 const router = express.Router();
 
 // routing
 // REGISTER || METHOD POST
 router.post('/register',registerController);
+
+//LOGIN || POST
+router.post('/login',loginController);
+
+// test routes
+router.get('/test',requireSignIn,isAdmin,testController);
 
 export default router;
